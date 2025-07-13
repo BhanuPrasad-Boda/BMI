9946c2dca8da9cfda71ff50f0320edd99af78438
BhanuPrasad-Boda <sivabhanuboda@gmail.com>
2025-07-13 15:59:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import { Bmi } from './components/bmi/bmi.jsx';
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
